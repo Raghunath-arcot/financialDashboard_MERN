d98928bc93b42f6e0ab6e8901af9002a12829855
Raghunath Rao Arcot <106887145+Raghunath-arcot@users.noreply.github.com>
2025-01-05 12:18:35 +0530

    Update expanded-theme.ts

--- client/src/expanded-theme.ts
+++ client/src/expanded-theme.ts
@@ -1,6 +1,6 @@
 
 // eslint-disable-next-line
-import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
+// import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
 
 declare module "@mui/material/styles/createPalette" {
     interface PaletteColor{
