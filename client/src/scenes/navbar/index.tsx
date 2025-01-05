a95c98e98abacbb17690825a79dfa1aee16644ae
Raghunath Rao Arcot <106887145+Raghunath-arcot@users.noreply.github.com>
2025-01-05 12:23:06 +0530

    Update index.tsx

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -4,9 +4,9 @@ import PixIcon from "@mui/icons-material/Pix";
 import { Box, Typography, useTheme } from '@mui/material';
 import FlexBetween from '../../components/FlexBetween';
 
-type Props = {};
 
-const Navbar = (props: Props) => {
+
+const Navbar = () => {
     const {palette} = useTheme();
     const [selected, setSelected] = useState("dashboard");
     return (
